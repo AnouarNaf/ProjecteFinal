ad001f600d0717025b053616161a18f7677afc34
julio <patatassaladaspepinillo@gmail.com>
2021-04-29 16:48:22 +0200

    cambios_cliente

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import Login from './login';
 ReactDOM.render(
   <React.StrictMode>
-    <Login/>
+    <App/>
   </React.StrictMode>,
 
   document.getElementById('root')
