74fc381ff90d452311818e353a4edf4544d467d1
JuanPa <jpmoldon@gxdevelopment.es>
2021-05-06 16:12:56 +0200

    Router

---
+++ src/Components/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import '../Styles/App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    BrowserRouter,
+    Redirect
+} from "react-router-dom";
+import Perfil from './Perfil';
+import Login from './login';
+import PrivateRoute from './PrivateRoute';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/login" component={ Login }/>
+                    <PrivateRoute exact path="/perfil" component={ Perfil }/>
+                    <Redirect to="/login"/>
+                </Switch>
+            </div>
+        </BrowserRouter>        
+    );
+}
+
+export default App;
